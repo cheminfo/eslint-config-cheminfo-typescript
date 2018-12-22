6f4bad7121cfb889f33cdb4d199d45c7ba25b651
Michaël Zasso <targos@protonmail.com>
2018-12-22 23:28:21 +0100

    feat: update eslint-plugin-typescript to 1.0.0-rc.0
    BREAKING CHANGE:
    
    Many more rules were enabled.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   extends: 'cheminfo',
-  parser: 'typescript-eslint-parser',
+  parser: 'eslint-plugin-typescript/parser',
   parserOptions: {
     sourceType: 'module'
   },
@@ -15,10 +15,47 @@ module.exports = {
     }
   },
   rules: {
-    'typescript/interface-name-prefix': ['error', 'always'], // Interfaces must have "I" prefix
-    'typescript/explicit-function-return-type': 'off', // Let TS infer the types by default
-    'typescript/no-unused-vars': 'error', // Needed for compatibility with the ESLint rule
-    'no-undef': 'off', // TypeScript flags those errors already
-    'valid-jsdoc': 'off' // TODO re-enable with different settings. For example, types are not necessary
+    'typescript/adjacent-overload-signatures': 'error',
+    'typescript/array-type': 'error',
+    'typescript/ban-types': 'error',
+    camelcase: 'off',
+    'typescript/camelcase': ['error', { properties: 'always' }],
+    'typescript/class-name-casing': 'error',
+    'typescript/explicit-function-return-type': [
+      'error',
+      { allowExpressions: true }
+    ],
+    'typescript/explicit-member-accessibility': 'error',
+    'typescript/generic-type-naming': 'off',
+    indent: 'off',
+    'typescript/indent': ['error', 2, { SwitchCase: 1 }],
+    'typescript/interface-name-prefix': ['error', 'always'],
+    'typescript/member-delimiter-style': 'error',
+    'typescript/member-naming': 'off',
+    'typescript/member-ordering': 'off',
+    'typescript/no-angle-bracket-type-assertion': 'error',
+    'typescript/no-array-constructor': 'error',
+    'typescript/no-empty-interface': 'error',
+    'typescript/no-explicit-any': 'error',
+    'typescript/no-extraneous-class': 'error',
+    'typescript/no-inferrable-types': 'error',
+    'typescript/no-misused-new': 'error',
+    'typescript/no-namespace': 'error',
+    'typescript/no-non-null-assertion': 'error',
+    'typescript/no-object-literal-type-assertion': 'error',
+    'typescript/no-parameter-properties': 'error',
+    'typescript/no-this-alias': 'error',
+    'typescript/no-triple-slash-reference': 'error',
+    'typescript/no-type-alias': 'off',
+    'no-unused-vars': 'off',
+    'typescript/no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false }
+    ],
+    'typescript/no-use-before-define': 'off',
+    'typescript/no-var-requires': 'error',
+    'typescript/prefer-interface': 'error',
+    'typescript/prefer-namespace-keyword': 'error',
+    'typescript/type-annotation-spacing': 'error'
   }
 };
