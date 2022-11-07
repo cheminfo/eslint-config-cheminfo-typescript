a514a41fdcadbda85b16146a613e8ce86c032634
Michaël Zasso <targos@protonmail.com>
2022-11-07 14:53:26 +0100

    fix: include TypeScript overrides in base config

--- jsdoc.js
+++ jsdoc.js
@@ -2,14 +2,4 @@
 
 module.exports = {
   extends: ['cheminfo/jsdoc'],
-  overrides: [
-    {
-      files: ['*.{ts,tsx,cts,mts}'],
-      rules: {
-        'jsdoc/require-param-type': 'off',
-        'jsdoc/require-returns-type': 'off',
-        'jsdoc/no-types': 'warn',
-      },
-    },
-  ],
 };
