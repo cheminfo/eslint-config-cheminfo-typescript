7c1810e5d9460b7176690765cfa83ef8ea4b41da
Michaël Zasso <targos@protonmail.com>
2024-05-01 10:41:55 +0200

    feat: add deprecation plugin (#45)
    Start with warn so we can assess impact.

--- test/test.mjs
+++ test/test.mjs
@@ -21,16 +21,32 @@ assert.strictEqual(
   'ok.js should have no warnings: ' + util.format(okResult.messages),
 );
 
-const errors = notOkResult.messages
-  .filter(isError)
-  .map((error) => error.ruleId)
-  .sort();
+const errors = notOkResult.messages.filter(isError).map(getRuleId).sort();
 assert.deepStrictEqual(errors, [
   '@typescript-eslint/array-type',
   '@typescript-eslint/no-non-null-assertion',
   '@typescript-eslint/no-unnecessary-type-assertion',
 ]);
 
+const warnings = notOkResult.messages
+  .filter(isWarning)
+  .filter(excludeJsdoc)
+  .map(getRuleId)
+  .sort();
+assert.deepStrictEqual(warnings, ['deprecation/deprecation']);
+
 function isError(message) {
   return message.severity === 2;
+}
+
+function isWarning(message) {
+  return message.severity === 1;
+}
+
+function excludeJsdoc(message) {
+  return !message.ruleId.startsWith('jsdoc/');
+}
+
+function getRuleId(message) {
+  return message.ruleId;
 }
