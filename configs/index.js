d699b936e7358c2a14e403819fd00356a107dda7
Michaël Zasso <targos@protonmail.com>
2025-07-08 10:28:36 +0200

    feat!: update base config and add Vitest config (#61)

--- configs/index.js
+++ configs/index.js
@@ -1,5 +1,6 @@
 import base from './base.js';
 import jsdoc from './jsdoc.js';
 import unicorn from './unicorn.js';
+import vitest from './vitest.js';
 
-export default [...base, ...jsdoc, ...unicorn];
+export default [...base, ...jsdoc, ...unicorn, ...vitest];
